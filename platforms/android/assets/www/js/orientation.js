7843d4e0e3c3a2681837ee63942d97460b98719d
Polymathronic <uros.milosevic@pupin.rs>
2014-11-21 16:51:46 +0100

    Search/filter box

---
+++ platforms/android/assets/www/js/orientation.js
@@ -0,0 +1,29 @@
+$(function(){
+	var compass = $('.compass-icon');
+	if(window.DeviceOrientationEvent) {
+
+	  window.addEventListener('deviceorientation', function(event) {
+			var alpha;
+			//Check for iOS property
+			if(event.webkitCompassHeading) {
+			  alpha = event.webkitCompassHeading;
+			  //Rotation is reversed for iOS
+			  compass.style.WebkitTransform = 'rotate(-' + alpha + 'deg)';
+			}
+			//non iOS
+			else {
+			  alpha = event.alpha;
+			  webkitAlpha = alpha;
+			  if(!window.chrome) {
+				//Assume Android stock
+				webkitAlpha = alpha-270;
+			  }
+			}
+
+			compass[0].style.Transform = 'rotate(' + alpha + 'deg)';
+			compass[0].style.WebkitTransform = 'rotate('+ webkitAlpha + 'deg)';
+			//Rotation is reversed for FF
+			compass[0].style.MozTransform = 'rotate(-' + alpha + 'deg)'; 
+		  }, false);
+	}
+});
