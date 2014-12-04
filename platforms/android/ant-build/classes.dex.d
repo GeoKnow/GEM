98c4083d8332231bef280991c0516a91e055a932
Polymathronic <uros.milosevic@pupin.rs>
2014-12-04 13:47:16 +0100

    Fixed icons and splash screen

---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+D:\Seafile\Workspace\IMP\Projects\Active\GeoKnow\Development\GEM\platforms\android\ant-build\classes.dex : \
+D:\Seafile\Workspace\IMP\Projects\Active\GeoKnow\Development\GEM\platforms\android\ant-build\classes\eu\geoknow\gem\BuildConfig.class \
+D:\Seafile\Workspace\IMP\Projects\Active\GeoKnow\Development\GEM\platforms\android\ant-build\classes\eu\geoknow\gem\gem.class \
+D:\Seafile\Workspace\IMP\Projects\Active\GeoKnow\Development\GEM\platforms\android\ant-build\classes\eu\geoknow\gem\R$attr.class \
+D:\Seafile\Workspace\IMP\Projects\Active\GeoKnow\Development\GEM\platforms\android\ant-build\classes\eu\geoknow\gem\R$drawable.class \
+D:\Seafile\Workspace\IMP\Projects\Active\GeoKnow\Development\GEM\platforms\android\ant-build\classes\eu\geoknow\gem\R$string.class \
+D:\Seafile\Workspace\IMP\Projects\Active\GeoKnow\Development\GEM\platforms\android\ant-build\classes\eu\geoknow\gem\R$xml.class \
+D:\Seafile\Workspace\IMP\Projects\Active\GeoKnow\Development\GEM\platforms\android\ant-build\classes\eu\geoknow\gem\R.class \
+D:\Seafile\Workspace\IMP\Projects\Active\GeoKnow\Development\GEM\platforms\android\ant-build\dexedLibs\classes-ca938cd286a74cc9407a4dff17ea59ab.jar \
