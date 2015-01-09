5596ecc10236f42c22b87a81859085f0d81cb983
Polymathronic <uros.milosevic@pupin.rs>
2015-01-09 09:54:50 +0100

    Cleaned up project

--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\Seafile\Workspace\IMP\Projects\Active\GeoKnow\Development\GEM\platforms\android\ant-build\AndroidManifest.xml : \
-D:\Seafile\Workspace\IMP\Projects\Active\GeoKnow\Development\GEM\platforms\android\AndroidManifest.xml \
--- platforms/android/assets/www/bower_components/jassa-ui-angular/README.md
+++
@@ -1,7 +0,0 @@
-Jassa-Angular-UI-Bower
-======================
-
-Bower Distribution of Jassa-UI-Angular - FOR RELEASES ONLY
-
-Source repository: https://github.com/GeoKnow/Jassa-UI-Angular
-
