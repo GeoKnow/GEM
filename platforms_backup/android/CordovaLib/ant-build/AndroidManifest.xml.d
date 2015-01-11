43c4699697bb3fbc5ab33e42bb60b4d55c265d19
Polymathronic <uros.milosevic@pupin.rs>
2015-01-11 15:49:06 +0100

    Minor fixes, project cleanup

--- platforms_backup/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\Seafile\Workspace\IMP\Projects\Active\GeoKnow\Development\GEM\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-D:\Seafile\Workspace\IMP\Projects\Active\GeoKnow\Development\GEM\platforms\android\CordovaLib\AndroidManifest.xml \
--- platforms_backup/android/CordovaLib/ant-gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : D:\Seafile\Workspace\IMP\Projects\Active\GeoKnow\Development\GEM\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
