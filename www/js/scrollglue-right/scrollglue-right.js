3c700eda262345bd7476c87ed59d13d51f588631
cstadler <cstadler@informatik.uni-leipzig.de>
2015-01-12 01:18:08 +0100

    messing around with the scroll glue - not working yet after a manual scroll

--- www/js/scrollglue-right/scrollglue-right.js
+++ www/js/scrollglue-right/scrollglue-right.js
@@ -88,7 +88,7 @@ angular.module('luegg.directives', [])
             scope.$watch(function() {
                 return el.clientWidth;
             }, function(w) {
-                console.log('client width: ', w);
+                console.log('client width: ', w, el.scrollWidth);
                 onScopeChanges();
             });
 
